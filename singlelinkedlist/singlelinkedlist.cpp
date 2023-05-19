1f1a2f0afa1c6accdfc156684feb9998a01b9c46
Iffatuz Zahra <iffatuz.zahra.ft22@mail.umy.ac.id>
2023-05-19 15:51:58 +0700

    mengecheck apakah node yang dimaksud ada didalam list

--- singlelinkedlist/singlelinkedlist.cpp
+++ singlelinkedlist/singlelinkedlist.cpp
@@ -91,5 +91,16 @@ bool List::delNode(int nim) //menghapus node dari dalam list
 
 	delete current;
 	return true;
-	
+}
+
+bool List::Search(int nim, Node** previous, Node** current)
+{
+	*previous = START;
+	*current = START;
+	while ((*current != NULL) && (nim != (*current)->noMhs))
+	{
+		*previous = *current;
+		*current = (*current)->next;
+	}
+	return (*current != NULL);
 }

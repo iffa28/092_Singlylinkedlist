60ecba3b5d6ebde8cccf46241020c1af6d3f4fb9
Iffatuz Zahra <iffatuz.zahra.ft22@mail.umy.ac.id>
2023-05-19 15:13:37 +0700

    Add project files.

---
+++ singlelinkedlist/singlelinkedlist.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string.h>
+using namespace std;
+
+class Node
+{
+public:
+	int noMhs;
+	char nama[20];
+	Node* next;
+};
+
+class List
+{
+	Node* START;
+public:
+	List();
+	void addNode();
+	bool Search(int nim, Node** current, Node** previous);
+	bool listEmpty();
+};
